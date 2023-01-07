c261910985543134924bef3ab359dff135c9a07c
Youssef Girgeis <youssef_girgeis@hotmail.com>
2023-01-06 21:51:29 -0500

    add more items to cart

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -19,6 +19,7 @@ export const Cart = (props) => {
     const newQuantity = Number(input);
 
     // Dispatch an action to change the quantity of the given name and quantity.
+    dispatch(changeItemQuantity(name, newQuantity));
   };
 
   // Use the cart and currencyFilter slices to render their data.
